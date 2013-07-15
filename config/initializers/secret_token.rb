8731974fe73d27f38af5f97bc2788b024a8f913a
Rudi Edelmann <rudicba@gmail.com>
2013-07-15 17:53:24 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sayri::Application.config.secret_token = '1fbe1473d282adc314241ad6df1abef0a44f75061123334262d5bda9a91aae447073b6f6a374d03541bf531fedbef96d6968d2b3090d28361afe8d5bfaefc2dc'
